a142a73c769bfc9caad5a3bf50e1b05b89d5b873
JeffreyRiggle <JeffreyRiggle@gmail.com>
2017-10-19 05:48:18 -0400

    animate notifications

--- src/notifications/notification-item.js
+++ src/notifications/notification-item.js
@@ -3,7 +3,9 @@ import {inject, bindable, customElement} from 'aurelia-framework';
 @customElement('notification-item')
 @bindable('notification')
 export class NotificationItem {
-    constructor() { }
+    constructor() { 
+        this.show = true;
+    }
 
     closeNotification() {
         this.notification.dismiss();
