bd5792eb65cdf0f5bc0ecf48130f315f796c0b0b
JeffreyRiggle <JeffreyRiggle@gmail.com>
2017-11-06 05:49:19 -0500

    add sorting

--- src/gridview/gridview.js
+++ src/gridview/gridview.js
@@ -1,4 +1,5 @@
 import {inject, bindable, customElement} from 'aurelia-framework';
+import _ from 'underscore';
 import './gridview.less';
 
 @bindable('options')
@@ -10,6 +11,7 @@ export class GridView {
     constructor(element) {
         this.element = element;
         this.selected = {};
+        this.sortProperty = '';
     }
 
     itemClicked(item) {
@@ -17,5 +19,35 @@ export class GridView {
 
         let e = new CustomEvent('selectedchanged', { detail: this.selected });
         this.element.dispatchEvent(e);
+    }
+
+    changeSort(column) {
+        var propertyChanged = this.sortProperty !== column.property;
+        if (propertyChanged) {
+            this.sortProperty = column.property;   
+        }
+
+        if (this.sortDirection === 'asc' && !propertyChanged) {
+            column.direction = 'dsc';
+        } else {
+            column.direction = 'asc';
+        }
+
+        this.sortDirection = column.direction;
+        this._applySort();
+    }
+
+    _applySort() {
+        if (!this.sortProperty || !this.sortDirection) {
+            return;
+        }
+
+        var temp = _.sortBy(this.items, this.sortProperty);
+
+        if (this.sortDirection === 'dsc') {
+            this.items = temp.reverse();
+        } else {
+            this.items =  temp;
+        }
     }
 }
