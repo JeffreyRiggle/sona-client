73726ba5be283953f5602a57bf18ef0d4bb94ffc
JeffreyRiggle <JeffreyRiggle@gmail.com>
2017-11-13 07:07:39 -0500

    add update/revert controls

--- src/incident/incident-viewer.js
+++ src/incident/incident-viewer.js
@@ -3,4 +3,38 @@ import {inject, bindable, customElement} from 'aurelia-framework';
 @customElement('incident-viewer')
 @bindable('incident')
 export class IncidentViewer {
+    constructor() {
+        this.initialValues = {};
+    }
+
+    updateIncident() {
+        this.incident.Update();
+        this._setupInitialValues(this.incident);
+    }
+
+    resetIncident() {
+        if (!this.incident || !this.initialValues) {
+            return;
+        }
+
+        this.incident.reporter = this.initialValues['reporter'];
+        this.incident.state = this.initialValues['state'];
+        this.incident.description = this.initialValues['description'];
+    }
+
+    incidentChanged(newValue, oldValue) {
+        this.initialValues = {};
+
+        if (!newValue) {
+            return;
+        }
+
+        this._setupInitialValues(newValue);
+    }
+
+    _setupInitialValues(incident) {
+        this.initialValues['reporter'] = incident.reporter;
+        this.initialValues['state'] = incident.state;
+        this.initialValues['description'] = incident.description;
+    }
 }
