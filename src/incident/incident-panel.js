561676402d6d4e3999d5d2710d69c440f23f3fbd
Jeff <JeffreyRiggle@gmail.com>
2020-01-21 06:51:10 -0500

    create a way to get to full screen view

--- src/incident/incident-panel.js
+++ src/incident/incident-panel.js
@@ -10,11 +10,13 @@ export class IncidentPanel {
         this.incidentmanager = IncidentManager;
         this.eventAg = EventAggregator;
         this.shown = !!this.incidentmanager.currentincident;
+        this.incidentRoute = `#/viewIncident/${this.incidentmanager.currentincident.id}`;
     }
 
     attached() {
         this.subscriber = this.eventAg.subscribe('incidentSelected', () => {
             this.shown = true;
+            this.incidentRoute = `#/viewIncident/${this.incidentmanager.currentincident.id}`;
         });
     }
 
