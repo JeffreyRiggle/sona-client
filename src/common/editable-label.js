21bd6eb5de8905f5c4ee8ef566c078c33fc56175
Jeff <JeffreyRiggle@gmail.com>
2020-01-23 06:35:26 -0500

    fix up editable text behavior

--- src/common/editable-label.js
+++ src/common/editable-label.js
@@ -7,6 +7,7 @@ import './editable-label.less';
 @inject(Element)
 export class EditableLabel {
     constructor(element) {
+        this.original = '';
         this.isEditing = false;
         this.element = element;
     }
@@ -16,11 +17,26 @@ export class EditableLabel {
 
         if (!this.isEditing) {
             this._raiseEdited();
+        } else {
+            this.original = this.item;
         }
     }
 
     _raiseEdited() {
         let e = new CustomEvent('edited', {detail: this.item });
         this.element.dispatchEvent(e);
+    }
+
+    handleKeyPress(event) {
+        if (event.key === 'Escape') {
+            this.item = this.original;
+            this.isEditing = false;
+        }
+
+        if (event.key === 'Enter') {
+            this.toggleIsEditing();
+        }
+
+        return true;
     }
 }
