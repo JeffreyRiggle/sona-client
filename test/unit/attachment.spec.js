c57f94bdfc59781a4dfd2d38dafda914af3caa16
Jeff <JeffreyRiggle@gmail.com>
2020-02-06 06:16:23 -0500

    attempt to fix failing unit test

--- test/unit/attachment.spec.js
+++ test/unit/attachment.spec.js
@@ -4,7 +4,7 @@ describe('Attachment', () => {
     let attachment;
 
     beforeEach(() => {
-        attachment = new Attachment('foo.txt', 1580903174652);
+        attachment = new Attachment('foo.txt', Date.now());
     });
     
     it('should have the correct name', () => {
@@ -12,6 +12,7 @@ describe('Attachment', () => {
     });
 
     it('should have the correct display date', () => {
-        expect(attachment.getFormatedTime()).toBe('5/1/2020 6:46');
+        const format = /\d{1,2}\/\d{1,2}\/\d{4}\s\d{1,2}:\d{2}/;
+        expect(format.test(attachment.getFormatedTime())).toBe(true);
     });
 });
