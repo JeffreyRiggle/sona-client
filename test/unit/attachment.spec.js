b42ea511a71834c3cf62a65c00c0eca573007643
Jeff <JeffreyRiggle@gmail.com>
2020-02-10 06:48:37 -0500

    add checkable dropdown tests

--- test/unit/attachment.spec.js
+++ test/unit/attachment.spec.js
@@ -12,7 +12,7 @@ describe('Attachment', () => {
     });
 
     it('should have the correct display date', () => {
-        const format = /\d{1,2}\/\d{1,2}\/\d{4}\s\d{1,2}:\d{2}/;
+        const format = /\d{1,2}\/\d{1,2}\/\d{4}\s\d{1,2}:\d{1,2}/;
         expect(format.test(attachment.getFormatedTime())).toBe(true);
     });
 });
