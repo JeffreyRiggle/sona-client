8b4fbd2bbc6ac63af3a82431a2b3ec8c6ffd5a0c
Jeff <JeffreyRiggle@gmail.com>
2020-02-18 21:11:55 -0500

    add tests for gitviewer

---
+++ test/unit/git-issue-viewer.spec.js
@@ -0,0 +1,56 @@
+import {bootstrap} from 'aurelia-bootstrapper';
+import {StageComponent} from 'aurelia-testing';
+import {PLATFORM} from 'aurelia-pal';
+import httpManager from '../../src/services/httpManager';
+describe('Stage Git Issue Viewer Component', () => {
+  let component, boundData, commentData, issueData;
+
+  beforeEach(() => {
+    issueData = {
+        closed_at: Date.now(),
+        title: 'Some issue',
+        state: 'Open',
+        user: {
+            login: 'reporter'
+        },
+        body: 'I am having an issue'
+    };
+
+    commentData = [{
+        body: 'This is a comment',
+        user: {
+            login: 'reporter'
+        },
+        created_at: Date.now(),
+    }];
+
+    httpManager.get = jest.fn((url) => {
+        if (/\/comments/.test(url)) {
+            return Promise.resolve(commentData);
+        }
+        return Promise.resolve(issueData);
+    });
+
+    boundData = {
+        issue: '/some/git/url'
+    };
+
+    component = StageComponent
+      .withResources(PLATFORM.moduleName('../../src/gitviewer/git-issue-viewer'))
+      .inView('<git-issue-viewer issue.bind="issue"></git-issue-viewer>')
+      .boundTo(boundData);
+  });
+
+  afterEach(() => component.dispose());
+
+  it('should render', done => {
+    component.create(bootstrap).then(() => {
+      const view = component.element.querySelector('h1');
+      expect(view.textContent.trim()).toBe('Some issue');
+      done();
+    }).catch(e => {
+      fail(e);
+      done();
+    });
+  });
+});
