7f0374bbb54a75ab0a33b111e308f247e328ccce
Jeff <JeffreyRiggle@gmail.com>
2020-03-08 13:35:33 -0400

    super basic testing

---
+++ test/conf.js
@@ -0,0 +1,10 @@
+exports.config = {
+    chromeDriver: '../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['e2e/auth.spec.js'],
+    capabilities: {
+        browserName: 'chrome',
+        maxInstances: 1,
+        count: 1
+    }
+};
