b5d8b8a38669f21fd1854cbe55628abf6b049b4a
Jeff <JeffreyRiggle@gmail.com>
2020-03-12 06:47:04 -0400

    get e2e tests running with gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const { src, series } = require('gulp');
+const { protractor } = require('gulp-protractor');
+const { spawn } = require('child_process');
+
+function updateWebDriver(callback) {
+    spawn('node_modules/protractor/bin/webdriver-manager', ['update', '--versions.chrome', '79.0.3945.88'], {
+        stdio: 'inherit'
+    }).once('close', callback);
+}
+
+function startWebDriver() {
+    return spawn('webdriver-manager', ['start'], {
+        stdio: 'inherit'
+    });;
+}
+
+function testE2E() {
+    return src(['./test/e2e/*.js'])
+      .pipe(protractor({
+          configFile: 'test/conf.js'
+      })).on('error', (e) => {
+          throw e;
+      });
+}
+
+exports.testE2E = series(updateWebDriver, testE2E);
--- test/conf.js
+++ test/conf.js
@@ -1,6 +1,5 @@
 exports.config = {
-    chromeDriver: '../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    chromeDriver: '../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_79.0.3945.88',
     specs: ['e2e/auth-spec.js'],
     capabilities: {
         browserName: 'chrome',
