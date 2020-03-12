10f7ed148282ca1f93bb4f43addea0160cb32174
Jeff <JeffreyRiggle@gmail.com>
2020-03-12 19:49:36 -0400

    create dev and ci modes

--- test/conf.js
+++ test/conf.js
@@ -1,10 +1,19 @@
+function chromeArgs() {
+    console.log(process.env);
+    if (process.env.TEST_MODE === 'ci') {
+        return ['--disable-dev-shm-usage', '--headless', '--no-sandbox'];
+    }
+
+    return [];
+}
+
 exports.config = {
     chromeDriver: '../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_79.0.3945.88',
     specs: ['e2e/auth-spec.js'],
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
-            args: ['--disable-dev-shm-usage', '--headless', '--no-sandbox']
+            args: chromeArgs()
         },
         maxInstances: 1,
         count: 1
