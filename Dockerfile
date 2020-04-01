8f8ed2a71ca84493504ba6b2b033f08165128a55
Jeff <JeffreyRiggle@gmail.com>
2020-04-01 17:02:37 -0400

    create fork

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM nginx
 
 COPY ./dist /site
 COPY nginx.tmpl /etc/nginx/nginx.tmpl
+COPY nginx.https.tmpl /etc/nginx/nginx.https.tmpl
 COPY start.sh /start.sh
 
 EXPOSE 80 443
--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 echo "Running script"
-envsubst < /etc/nginx/nginx.tmpl > /etc/nginx/nginx.conf && nginx -g 'daemon off;' || cat /etc/nginx/nginx.conf
+echo $SONA_URL
+
+if [ -z "${HTTPS}" ]
+then
+    echo "Using http config"
+    envsubst < /etc/nginx/nginx.tmpl > /etc/nginx/nginx.conf && nginx -g 'daemon off;' || cat /etc/nginx/nginx.conf
+else
+    echo "Using https config"
+    envsubst < /etc/nginx/nginx.https.tmpl > /etc/nginx/nginx.conf && nginx -g 'daemon off;' || cat /etc/nginx/nginx.conf
+fi
