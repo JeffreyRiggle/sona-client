1cbb51da592c397f6dce47289e5a3648905eeca4
Jeff <JeffreyRiggle@gmail.com>
2020-04-03 07:58:36 -0400

    get https working in docker image

--- start.sh
+++ start.sh
@@ -3,11 +3,16 @@
 echo "Running script"
 echo $SONA_URL
 
-if [ -z "${HTTPS}" ]
+if [ -z "${CERT}" ]
 then
     echo "Using http config"
     envsubst < /etc/nginx/nginx.tmpl > /etc/nginx/nginx.conf && nginx -g 'daemon off;' || cat /etc/nginx/nginx.conf
 else
     echo "Using https config"
+    echo "${CERT}" > temp
+    base64 -d temp > /etc/nginx/server.crt
+
+    echo "${KEY}" > temp
+    base64 -d temp > /etc/nginx/server.key
     envsubst < /etc/nginx/nginx.https.tmpl > /etc/nginx/nginx.conf && nginx -g 'daemon off;' || cat /etc/nginx/nginx.conf
 fi
